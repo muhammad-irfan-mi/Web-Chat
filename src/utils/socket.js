06983f99bc15d576e2db6590cbb554210a7d4bdb
muhammad-irfan-mi <irfanmalikhu@gmail.com>
2025-09-01 16:00:23 +0500

    changes

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,28 +1,28 @@
-import { io } from "socket.io-client";
+// import { io } from "socket.io-client";
 
-let socket = null;
+// let socket = null;
 
-export const initSocket = (conversationId, token) => {
-  if (socket) {
-    socket.disconnect();
-  }
+// export const initSocket = (conversationId, token) => {
+//   if (socket) {
+//     socket.disconnect();
+//   }
 
-  socket = io('wss://ddd80f4f8433.ngrok-free.app', {
-    transports: ["websocket"],
-    query: {
-      conversationId,
-      token
-    }
-  });
+//   socket = io('wss://ddd80f4f8433.ngrok-free.app', {
+//     transports: ["websocket"],
+//     query: {
+//       conversationId,
+//       token
+//     }
+//   });
 
-  return socket;
-};
+//   return socket;
+// };
 
-export const getSocket = () => socket;
+// export const getSocket = () => socket;
 
-export const disconnectSocket = () => {
-  if (socket) {
-    socket.disconnect();
-    socket = null;
-  }
-};
+// export const disconnectSocket = () => {
+//   if (socket) {
+//     socket.disconnect();
+//     socket = null;
+//   }
+// };
